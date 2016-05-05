6594e5fc2391f382505aa871525f91ec04bae1c1
ntnguyenphat <ntp.nguyenphat@gmail.com>
2016-05-05 21:05:14 +0700

    Create framwork and coding conventions

--- SeleniumAdvance/Common/Constant.cs
+++ SeleniumAdvance/Common/Constant.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
 
-namespace SeleniumAdvance.TestCases
+namespace SeleniumAdvance.Common
 {
-    class Class1
+    public class Constant
     {
+        public static IWebDriver WebDriver;
     }
 }
